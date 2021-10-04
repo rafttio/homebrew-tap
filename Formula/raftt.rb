4d4312e62a7b67895ec667a6ab29c228b6ae4e2f
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-04 17:57:45 +0300

    Brew formula update for raftt version v18.0.2

--- Formula/raftt.rb
+++ Formula/raftt.rb
@@ -5,20 +5,20 @@
 class Raftt < Formula
   desc "Manage your development environment with ease"
   homepage "https://raftt.io/"
-  version "18.0.1"
+  version "18.0.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v18.0.1/raftt_18.0.1_darwin_amd64.tar.gz"
-      sha256 "da12b7afe841667b02a33c1f61970fd5eb63b75ae90f01e2b497122381922c22"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v18.0.2/raftt_18.0.2_darwin_amd64.tar.gz"
+      sha256 "f839ac43135e9c6f81241cc464950617d7a5d142803037b7e237a50c6ac90eb1"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v18.0.1/raftt_18.0.1_linux_amd64.tar.gz"
-      sha256 "52049f6820d2a6bd2928fce04a2472b735ad06eaf2306a6bc979ae07398fc69f"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v18.0.2/raftt_18.0.2_linux_amd64.tar.gz"
+      sha256 "322d6fe793a478fe5196d9c9c63edf5f0f4bce8bb184fd62a613ef08b32ac4a5"
     end
   end
 

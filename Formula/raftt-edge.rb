2bb8401ca447e61ea1e161a92f91f3dd66b05058
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-26 18:19:36 +0300

    Brew formula update for raftt version v47.0.4

--- Formula/raftt-edge.rb
+++ Formula/raftt-edge.rb
@@ -5,12 +5,12 @@
 class RafttEdge < Formula
   desc "Manage your development environment with ease - edge version, might be unstable"
   homepage "https://raftt.io/"
-  version "47.0.2"
+  version "47.0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.2/raftt_47.0.2_darwin_arm64.tar.gz"
-      sha256 "daef9f66607b087adca6e745c5733e0df045a05662c61bf949438dc81f500186"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.4/raftt_47.0.4_darwin_arm64.tar.gz"
+      sha256 "6fb4272c98ac15e11dad3ba5eb21f6f7a350be44b2fdd734f63b0666731cb712"
 
       def install
         bin.install "raftt"
@@ -29,8 +29,8 @@ class RafttEdge < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.2/raftt_47.0.2_darwin_amd64.tar.gz"
-      sha256 "006f234a449361ca42f9edf0ae1d0b6b1b0b5a477ed63a943185446421255fd3"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.4/raftt_47.0.4_darwin_amd64.tar.gz"
+      sha256 "2b6f59f30ecedbf932e6a310ba0efccb2091e254f7f8578bf75af1e3c51bcee4"
 
       def install
         bin.install "raftt"
@@ -52,8 +52,8 @@ class RafttEdge < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.2/raftt_47.0.2_linux_amd64.tar.gz"
-      sha256 "d0204619d48672367009b1910d2d0de3e80f5ed64cc6f3ebfa80451bbeceb1df"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v47.0.4/raftt_47.0.4_linux_amd64.tar.gz"
+      sha256 "23605ecb3d2040539dd3a32996a14b34674626871a9b411dafa92cd3e5f16c2c"
 
       def install
         bin.install "raftt"

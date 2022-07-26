c3841a36086fa55aadd170f8ff7e0b457db96dd2
goreleaserbot <goreleaser@carlosbecker.com>
2022-07-26 12:06:54 +0300

    Brew formula update for raftt version v56.0.1

--- Formula/raftt-edge.rb
+++ Formula/raftt-edge.rb
@@ -5,12 +5,12 @@
 class RafttEdge < Formula
   desc "Manage your development environment with ease - edge version, might be unstable"
   homepage "https://raftt.io/"
-  version "56.0.0"
+  version "56.0.1"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.0/raftt_56.0.0_darwin_amd64.tar.gz"
-      sha256 "4ebad283a5c44d952c151d5b051e803ff628eb9e2f80a4031d660dc94286a0d0"
+    if Hardware::CPU.arm?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.1/raftt_56.0.1_darwin_arm64.tar.gz"
+      sha256 "44d2c18d5949406a15233368f55a22fe8b75b6661029bf7d0f4498b74c0b8e74"
 
       def install
         bin.install "raftt"
@@ -28,9 +28,9 @@ class RafttEdge < Formula
         fish_completion.install "fish_completion.fish" => "raftt.fish"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.0/raftt_56.0.0_darwin_arm64.tar.gz"
-      sha256 "6ea30ffb91d514641e80ed7f68ec36c1296da6d596366ab1d890db9747b4d8c5"
+    if Hardware::CPU.intel?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.1/raftt_56.0.1_darwin_amd64.tar.gz"
+      sha256 "a3961cc026de4100697992bacb743b5766276d90772a3450526cfed8c7418aa6"
 
       def install
         bin.install "raftt"
@@ -52,8 +52,8 @@ class RafttEdge < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.0/raftt_56.0.0_linux_amd64.tar.gz"
-      sha256 "6779dab139b76d886bc1e1d45954d5675ae9361637e31c274fbef42f90c85e8e"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v56.0.1/raftt_56.0.1_linux_amd64.tar.gz"
+      sha256 "7babdda01e7548969c3fa77d3314b114572ed33088d1f035c0687ab538241804"
 
       def install
         bin.install "raftt"

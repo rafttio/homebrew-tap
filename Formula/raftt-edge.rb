a0826e0046a8c472c286ce2e2f507af913a48ce3
goreleaserbot <goreleaser@carlosbecker.com>
2022-11-14 11:48:46 +0200

    Brew formula update for raftt version v71.1.0

--- Formula/raftt-edge.rb
+++ Formula/raftt-edge.rb
@@ -5,14 +5,14 @@
 class RafttEdge < Formula
   desc "Manage your development environment with ease - edge version, might be unstable"
   homepage "https://raftt.io/"
-  version "70.3.0"
+  version "71.1.0"
 
   depends_on "bash"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v70.3.0/raftt_70.3.0_darwin_arm64.tar.gz"
-      sha256 "58a04a50d3b598a24de50613fd4d9a1de3df3e9bae6a9f29b44ddaa4269d6955"
+    if Hardware::CPU.intel?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v71.1.0/raftt_71.1.0_darwin_amd64.tar.gz"
+      sha256 "388e37226894dd6765769060e61d5b84be43b5a4bc2da59ff0bb036f4ccd9b42"
 
       def install
         bin.install "raftt"
@@ -30,9 +30,9 @@ class RafttEdge < Formula
         fish_completion.install "fish_completion.fish" => "raftt.fish"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v70.3.0/raftt_70.3.0_darwin_amd64.tar.gz"
-      sha256 "ca4f74cdf933d32f74f012d75463a18507de758e032b857aa27f57e5b8464ecf"
+    if Hardware::CPU.arm?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v71.1.0/raftt_71.1.0_darwin_arm64.tar.gz"
+      sha256 "579e9b5ef7a4b5c5c0c289c82f00604768f10aaed6e16e81eccec56865580792"
 
       def install
         bin.install "raftt"
@@ -54,8 +54,8 @@ class RafttEdge < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v70.3.0/raftt_70.3.0_linux_amd64.tar.gz"
-      sha256 "eba8bab51693688c801d2f908366b02107b246769dee8daa8ad114c4296d7cd3"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v71.1.0/raftt_71.1.0_linux_amd64.tar.gz"
+      sha256 "ef8966e9e5509d899d3c2248f7f3034e8f5d0c367e394df6204230b672fab483"
 
       def install
         bin.install "raftt"

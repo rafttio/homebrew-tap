4f0451fdb8e045905301e16cbd219b657b179d37
goreleaserbot <goreleaser@carlosbecker.com>
2022-12-12 23:32:53 +0200

    Brew formula update for raftt version v75.0.2

--- Formula/raftt-edge.rb
+++ Formula/raftt-edge.rb
@@ -5,14 +5,14 @@
 class RafttEdge < Formula
   desc "Manage your development environment with ease - edge version, might be unstable"
   homepage "https://raftt.io/"
-  version "74.1.2"
+  version "75.0.2"
 
   depends_on "bash"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v74.1.2/raftt_74.1.2_darwin_amd64.tar.gz"
-      sha256 "89e06ff596a89fa8c0aa491c350e26121a1722e060c0e566eaf32a5937cab2d6"
+    if Hardware::CPU.arm?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v75.0.2/raftt_75.0.2_darwin_arm64.tar.gz"
+      sha256 "b1b495a27ee7ed3ae8628b706c7ff8daed407a9a0c9c3c7f23f1597348111662"
 
       def install
         bin.install "raftt"
@@ -30,9 +30,9 @@ class RafttEdge < Formula
         fish_completion.install "fish_completion.fish" => "raftt.fish"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v74.1.2/raftt_74.1.2_darwin_arm64.tar.gz"
-      sha256 "286967bd0ebe0969dbc22531ff4771ce6bc938c20526e8e8d8f81f8396179f7d"
+    if Hardware::CPU.intel?
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v75.0.2/raftt_75.0.2_darwin_amd64.tar.gz"
+      sha256 "8261412b03c61987bfd8b30214a01b9d8c38a7084be52d89354635978f02f743"
 
       def install
         bin.install "raftt"
@@ -54,8 +54,8 @@ class RafttEdge < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v74.1.2/raftt_74.1.2_linux_amd64.tar.gz"
-      sha256 "9d2d8cba1344c023d807e09c8a8bdc6e90b79bc02ec468785e84b930cfa52af7"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v75.0.2/raftt_75.0.2_linux_amd64.tar.gz"
+      sha256 "5174579f80977c9f0e8a029f7e60b00be02a9be7b5cf5a7eb64dff742e7d03e7"
 
       def install
         bin.install "raftt"

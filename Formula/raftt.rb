f03454e9b35f41c58c1cf674bfc215c097470698
Idan Katz <idanski1@gmail.com>
2023-06-13 17:25:30 +0300

    v100.6.0 to stable

--- Formula/raftt.rb
+++ Formula/raftt.rb
@@ -5,14 +5,14 @@
 class Raftt < Formula
   desc "Manage your development environment with ease"
   homepage "https://raftt.io/"
-  version "100.4.0"
+  version "100.6.0"
 
   depends_on "bash"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.4.0/raftt_100.4.0_darwin_arm64.tar.gz"
-      sha256 "05b1b3557e1b8a79485880dc1d9ef75f3dab97307fb37427c96f1915f55d372c"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.6.0/raftt_100.6.0_darwin_arm64.tar.gz"
+      sha256 "87fa7629ee93ffab81bd00a20441a19199b8de2242d1e535dee38ad657833181"
 
       def install
         bin.install "raftt"
@@ -31,8 +31,8 @@ class Raftt < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.4.0/raftt_100.4.0_darwin_amd64.tar.gz"
-      sha256 "1da603d58c5316325ce71c6706a9ddb2e2a9e465a294876419b2bdee6009fb9a"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.6.0/raftt_100.6.0_darwin_amd64.tar.gz"
+      sha256 "332b40efcd14ea99cf1f7fb7eb2a7983561bef9f4b754ae364a334bd3d0db3e4"
 
       def install
         bin.install "raftt"
@@ -54,8 +54,8 @@ class Raftt < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.4.0/raftt_100.4.0_linux_amd64.tar.gz"
-      sha256 "d98e80cd03601a4fbd2fd0fb263e9441d8293c4382c930abcd7cafcdc2a238b7"
+      url "https://raftt-resources.s3.eu-central-1.amazonaws.com/cli/v100.6.0/raftt_100.6.0_linux_amd64.tar.gz"
+      sha256 "ae257ce8ddf388ba581a9530812a3b873ad2e05be545a99db3485b9875d3c5f7"
 
       def install
         bin.install "raftt"
